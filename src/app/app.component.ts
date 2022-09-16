b27831150a66c55c1cf7227729f60f8eb90fc74d
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-09-16 19:21:50 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+0import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
